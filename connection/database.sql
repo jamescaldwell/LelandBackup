0512d1ef8c06d6b515fed70ccbd9de51c524082f
Jim Caldwell <jim.caldwell.99@gmail.com>
2024-10-18 20:48:37 -0500

    First pass of complete code for backend CRUD

---
+++ connection/database.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS library;
+
+USE library;
+
+CREATE TABLE author(
+    author_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255),
+    bio TEXT,
+    INDEX name_index (name)
+);
+
+CREATE TABLE book(
+    book_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    description TEXT,
+    author_id INT,
+    pubdate DATE,
+    INDEX auth_id (author_id),
+    FOREIGN KEY (author_id) REFERENCES author (author_id) ON DELETE CASCADE,
+    INDEX title_index (title)
+);
---
+++ routes/itemroutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+
+const { createAuthor, getAllAuthors, getAuthorById, updateAuthor, deleteAuthor, 
+  createBook,  getAllBooks, getBookById, updateBook, deleteBook} = require("../controller/libraryController")
+
+
+router.post("/api/v1/author", createAuthor);
+router.get("/api/v1/author", getAllAuthors);
+router.get("/api/v1/author/:author_id", getAuthorById);
+router.put("/api/v1/author/:author_id", updateAuthor);
+router.delete("/api/v1/author/:author_id", deleteAuthor);
+
+router.post("/api/v1/book", createBook);
+router.get("/api/v1/book", getAllBooks);
+router.get("/api/v1/book/:book_id", getBookById);
+router.put("/api/v1/book/:book_id", updateBook);
+router.delete("/api/v1/book/:book_id", deleteBook);
+
+module.exports = router;
